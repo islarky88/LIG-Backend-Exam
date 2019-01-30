cfd75b57f4f9b89ef2532a867a0685d22581590a
Ace Maranga <islarky88@gmail.com>
2019-01-30 13:54:37 +0800

    Trying to add login API

--- api/api.php
+++ api/api.php
@@ -1,7 +1,8 @@
 <?php
 require_once('includes.php');
 $debug = 0;
-
+$response = '';
+$rawreponse = '';
 
 function testRequest() {
 
@@ -10,24 +11,26 @@ function testRequest() {
     echo $_SERVER['REQUEST_METHOD'];
 
 }
-if (isset($_GET['$key1'])) {
-  $key1 = trim(preg_replace('/[^A-Za-z0-9-]/','',$_GET['$key1']));
+
+if (isset($_GET['key1'])) {
+  $key1 = trim(preg_replace('/[^A-Za-z0-9-]/','',$_GET['key1']));
 }
-if (isset($_GET['$key2'])) {
-  $key2 = trim(preg_replace('/[^A-Za-z0-9-]/','',$_GET['$key2']));
+if (isset($_GET['key2'])) {
+  $key2 = trim(preg_replace('/[^A-Za-z0-9-]/','',$_GET['key2']));
 }
-if (isset($_GET['$key3'])) {
-  $key3 = trim(preg_replace('/[^A-Za-z0-9-]/','',$_GET['$key3']));
+if (isset($_GET['key3'])) {
+  $key3 = trim(preg_replace('/[^A-Za-z0-9-]/','',$_GET['key3']));
 }
-if (isset($_GET['$key4'])) {
-  $key4 = trim(preg_replace('/[^A-Za-z0-9-]/','',$_GET['$key4']));
+if (isset($_GET['key4'])) {
+  $key4 = trim(preg_replace('/[^A-Za-z0-9-]/','',$_GET['key4']));
 }
 
 
-if (isset($_SERVER['REQUEST_METHOD'])) {
+
 
   $method = $_SERVER['REQUEST_METHOD'];
 
+// START OF CHECK OF REQUEST METHOD TO IDENTIFY API TYPE
 
   if ($method === 'POST') {
 
@@ -37,14 +40,77 @@ if (isset($_SERVER['REQUEST_METHOD'])) {
       $response['username'] = 'user';
       $response['password'] = 'pass';
 
+      print_r($_REQUEST);
+
+    $rawreponse = array (
+        'message' => 'The given data was invalid.',
+        'errors' =>
+        array (
+          'email' =>
+          array (
+            0 => 'The email field is required.',
+          ),
+          'password' =>
+          array (
+            0 => 'The password field is required.',
+          ),
+        ),
+      );
+
+
+
+
 
     }
 
+
+
+
   } else if ($method === 'GET') {
 
+    if ($key1 == 'posts') {
 
 
-print_r($_REQUEST);
+      $rawreponse = array (
+      'data' =>
+      array (
+        0 =>
+        array (
+          'id' => 1,
+          'user_id' => 1,
+          'title' => 'new title',
+          'slug' => 'new-title',
+          'content' => 'content of the post',
+          'created_at' => '2019-01-23 02:06:06',
+          'updated_at' => '2019-01-23 02:13:26',
+          'deleted_at' => NULL,
+        ),
+      ),
+      'links' =>
+      array (
+        'first' => 'http://127.0.0.1:8000/api/posts?page=1',
+        'last' => 'http://127.0.0.1:8000/api/posts?page=1',
+        'prev' => NULL,
+        'next' => NULL,
+      ),
+      'meta' =>
+      array (
+        'current_page' => 1,
+        'from' => 1,
+        'last_page' => 1,
+        'path' => 'http://127.0.0.1:8000/api/posts',
+        'per_page' => 15,
+        'to' => 1,
+        'total' => 1,
+      ),
+    );
+
+     $response = json_encode($rawreponse);
+
+
+   } //end of if posts
+
+//print_r($_REQUEST);
 
 
 
@@ -54,7 +120,13 @@ print_r($_REQUEST);
 
   }
 
+  // Output JSON response
+  if ($response != '') {
+    echo $response;
+  } else {
+    print_r($_REQUEST);
+  }
 
-}
+
 
  ?>
